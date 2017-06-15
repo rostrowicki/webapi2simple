8d11eb7bd2d606e73f0c91ddda2d6a2f8f28b3bb
Robert Ostrowicki <Robert Ostrowicki>
2017-06-15 13:22:46 +0200

    Dodaj pliki projektów.

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product {Id=1, Name="Tomato Soup", Category="Grocieries", Price=2.5M},
+            new Product {Id=2, Name="Yo-yo", Category="Toys", Price = 3.75M},
+            new Product {Id=3, Name="Hammer", Category="Hardware", Price=16.99M}
+        };
+
+
+        // GET: Products
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
